b95707141c50e61d5a56bc5cd494fa3c742a259b
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-03-16 15:21:21 -0700

    first table post and get working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Housewife = require('../lib/models/Housewife');
 
 describe('backend-hands-of-resources routes', () => {
   beforeEach(() => {
@@ -10,5 +11,21 @@ describe('backend-hands-of-resources routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  // real housewives table
+  it('creates a housewive', async () => {
+    const expected = {
+      name: 'Kim Richards',
+      season: 'beverly hills',
+    };
+    const res = await request(app).post('/api/v1/housewives').send(expected);
+    expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('gets a list of housewives', async () => {
+    const expected = await Housewife.findAll();
+    const res = await request(app).get('/api/v1/housewives');
+    expect(res.body).toEqual(expected);
   });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,12 @@
--- Use this file to define your SQL tables
--- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS housewives; 
+
+CREATE TABLE housewives (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+    name TEXT NOT NULL,
+    season TEXT NOT NULL
+); 
+
+INSERT INTO
+    housewives (name, season)
+VALUES
+    ('Lisa Vanderpump', 'Beverly Hills')
