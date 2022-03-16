62661ca4dab530d9298ae50f4df527fdc38628fc
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-03-16 15:34:56 -0700

    first table findById working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -28,4 +28,10 @@ describe('backend-hands-of-resources routes', () => {
     const res = await request(app).get('/api/v1/housewives');
     expect(res.body).toEqual(expected);
   });
+
+  it('gets a housewife by id', async () => {
+    const expected = await Housewife.findById(1);
+    const res = await request(app).get('/api/v1/housewives/1');
+    expect(res.body).toEqual(expected);
+  });
 });
