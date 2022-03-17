95c7ea65770f6a757d802aca7a3d30b9ab403d7a
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-03-17 09:51:37 -0700

    find all working in friend table

--- __tests__/friends.test.js
+++ __tests__/friends.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Friend = require('../lib/models/Friend');
 
 describe('backend-hands-of-resources routes, friends table', () => {
   beforeEach(() => {
@@ -21,7 +22,12 @@ describe('backend-hands-of-resources routes, friends table', () => {
     const res = await request(app).post('/api/v1/friends').send(expected);
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
   });
-  // post
+
+  it('gets a list of friends', async () => {
+    const expected = await Friend.findAll();
+    const res = await request(app).get('/api/v1/friends');
+    expect(res.body).toEqual(expected);
+  });
   // get all
   // get by id
   // update
