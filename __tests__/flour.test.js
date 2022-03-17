db7ef065d18bc44fb298d0f8e21167e14317292a
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-03-17 11:14:38 -0700

    get all working in flours table

--- __tests__/flour.test.js
+++ __tests__/flour.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Flour = require('../lib/models/Flour');
 
 describe('backend-hands-of-resources routes, flour table', () => {
   beforeEach(() => {
@@ -19,5 +20,11 @@ describe('backend-hands-of-resources routes, flour table', () => {
     };
     const res = await request(app).post('/api/v1/flours').send(expected);
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('finds a list of flours', async () => {
+    const expected = await Flour.findAll();
+    const res = await request(app).get('/api/v1/flours');
+    expect(res.body).toEqual(expected);
   });
 });
