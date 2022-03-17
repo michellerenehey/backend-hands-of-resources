28891a9ff9283db172269cf59d4344bae57791e8
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-03-17 10:06:18 -0700

    find by id error working in friend table

--- __tests__/friends.test.js
+++ __tests__/friends.test.js
@@ -34,6 +34,14 @@ describe('backend-hands-of-resources routes, friends table', () => {
     const res = await request(app).get(`/api/v1/friends/${expected.id}`);
     expect(res.body).toEqual(expected);
   });
+
+  it('returns 404 error if no friend id', async () => {
+    const res = await request(app).get(
+      '/api/v1/friends/fake-id-does-not-exist'
+    );
+    expect(res.status).toEqual(404);
+  });
+
   // get by id
   // update
   // delete
