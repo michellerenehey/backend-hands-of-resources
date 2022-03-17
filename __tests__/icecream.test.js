c6039ce41ec749ab4667cac9880474a7c68c0668
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-03-17 12:29:35 -0700

    findAll working in ice cream table

--- __tests__/icecream.test.js
+++ __tests__/icecream.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const IceCream = require('../lib/models/IceCream');
 
 describe('backend-hands-of-resources routes, icecream table', () => {
   beforeEach(() => {
@@ -19,5 +20,11 @@ describe('backend-hands-of-resources routes, icecream table', () => {
     };
     const res = await request(app).post('/api/v1/icecream').send(expected);
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('gets a list of icecream', async () => {
+    const expected = await IceCream.findAll();
+    const res = await request(app).get('/api/v1/icecream');
+    expect(res.body).toEqual(expected);
   });
 });
